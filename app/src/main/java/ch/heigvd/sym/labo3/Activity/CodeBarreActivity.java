a12eee67c47775342f7a81688101b554479d9d8b
isaia.spinelli <isaia.spinelli@heig-vd.ch>
2019-11-15 14:10:18 +0100

    test mais retourne null

--- app/src/main/java/ch/heigvd/sym/labo3/Activity/CodeBarreActivity.java
+++ app/src/main/java/ch/heigvd/sym/labo3/Activity/CodeBarreActivity.java
@@ -1,8 +1,14 @@
 package ch.heigvd.sym.labo3.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import ch.heigvd.sym.labo3.R;
 
@@ -10,13 +16,64 @@ import ch.heigvd.sym.labo3.R;
  * @Class       : NFCActivity
  * @Author(s)   : Spinelli Isaia et Simonet Yoann
  * @Date        : 15.11.2019
+ * @Reference : https://github.com/journeyapps/zxing-android-embedded#adding-aar-dependency-with-gradle
  */
 public class CodeBarreActivity extends AppCompatActivity {
+
+    private TextView resCodeBarre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_code_barre);
 
+        IntentIntegrator integrator = new IntentIntegrator(this);
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.ONE_D_CODE_TYPES);
+        integrator.setPrompt("Scan a barcode");
+        integrator.setCameraId(0);  // Use a specific camera of the device
+        integrator.setBeepEnabled(false);
+        integrator.setBarcodeImageEnabled(true);
+        integrator.initiateScan();
+
+
+
+       /* try {
+
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+
+            startActivityForResult(intent, 0);
+
+        } catch (Exception e) {
+
+            Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+            startActivity(marketIntent);
+
+        }*/
+
+
+        resCodeBarre = findViewById(R.id.resCodeBarre);
+        Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+        resCodeBarre.setText("Hi!");
+
+       // while(true);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        Toast.makeText(this, "Nice !", Toast.LENGTH_LONG).show();
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if(scanResult != null) {
+            if(scanResult.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + scanResult.getContents(), Toast.LENGTH_LONG).show();
+                resCodeBarre.setText(scanResult.getContents());
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, intent);
+        }
     }
 }
