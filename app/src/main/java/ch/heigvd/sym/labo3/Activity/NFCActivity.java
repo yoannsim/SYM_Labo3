51fc9c6c4ee364e1f8bf66412f5bd169d28aa5dd
isaia.spinelli <isaia.spinelli@heig-vd.ch>
2019-11-15 11:01:30 +0100

    Init labo with activity

---
+++ app/src/main/java/ch/heigvd/sym/labo3/Activity/NFCActivity.java
@@ -0,0 +1,22 @@
+package ch.heigvd.sym.labo3.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import ch.heigvd.sym.labo3.R;
+
+/**
+ * @Class       : NFCActivity
+ * @Author(s)   : Spinelli Isaia et Simonet Yoann
+ * @Date        : 15.11.2019
+ */
+public class NFCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc);
+
+    }
+}
--- app/src/main/java/ch/heigvd/sym/labo3/MainActivity.java
+++ app/src/main/java/ch/heigvd/sym/labo3/MainActivity.java
@@ -2,13 +2,38 @@ package ch.heigvd.sym.labo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
+import ch.heigvd.sym.labo3.Activity.*;
+
+/**
+ * @Class       : MainActivity
+ * @Author(s)   : Spinelli Isaia et Simonet Yoann
+ * @Date        : 15.11.2019
+ */
 public class MainActivity extends AppCompatActivity {
+
+    Button btnNFC;
+    Button btnCodeBare;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnNFC = findViewById(R.id.ButtonNFC);
+        btnNFC.setOnClickListener(v -> {
+            Intent intent = new Intent(btnNFC.getContext(), NFCActivity.class);
+            btnNFC.getContext().startActivity(intent);
+        });
+
+        btnCodeBare = findViewById(R.id.buttonCodeBarre);
+
+        btnCodeBare.setOnClickListener(v -> {
+            Intent intent = new Intent(btnCodeBare.getContext(), CodeBarreActivity.class);
+            btnCodeBare.getContext().startActivity(intent);
+        });
     }
 }
